a9f2130af2c8d790bdb4d4f90abc40440b548792
Pranav Kumar <pk2051872@gmail.com>
2026-08-18 07:12:02 +0530

    GFG POTD - Snake and Ladder Problem
    Implement BFS approach for Snake and Ladder problem.

---
+++ 17_Aug_2026.py
@@ -0,0 +1,41 @@
+# GFG POTD - 2026-08-18
+# Snake and Ladder Problem
+# Approach: BFS
+
+from collections import deque
+class Solution:
+    def minThrows(self, n, lad, sn):
+        # code here
+        visited = {1}
+        sn_lad = {}
+        k = len(lad)
+        m = len(sn)
+        
+        for i in range(0, k, 2):
+            sn_lad[lad[i]] = lad[i+1]
+            
+        for i in range(0, m, 2):
+            sn_lad[sn[i]] = sn[i+1]
+            
+        queue = deque([(1,0)])
+        
+        while queue:
+            cell, throws = queue.popleft()
+            
+            for dice in range(1,7):
+                next_cell = cell + dice
+                
+                if next_cell > n * n:
+                    continue
+                
+                if next_cell in sn_lad:
+                    next_cell = sn_lad[next_cell]
+                    
+                if next_cell == n * n:
+                    return throws + 1
+                    
+                if next_cell not in visited:
+                    visited.add(next_cell)
+                    
+                    queue.append((next_cell, throws + 1))
+        return -1

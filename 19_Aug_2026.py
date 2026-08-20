f40e0990b3c10d380f2c9833d0e47d84849dc2db
Pranav Kumar <pk2051872@gmail.com>
2026-08-20 15:31:05 +0530

    GFG POTD - Node and Ancestor Max Diff
    Implement maxDiff method to calculate maximum difference between node and its ancestors in a binary tree.

---
+++ 19_Aug_2026.py
@@ -0,0 +1,33 @@
+# GFG POTD - 2026-08-19
+# Node and Ancestor Max Diff
+# Approach: KMP (LPS) + Greedy Compression
+
+''' Structure of Binary Tree Node
+class Node:
+    def __init__(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
+class Solution:
+    def maxDiff(self, root):
+        # Code here 
+        ans = float('-inf')
+
+        def dfs(node, max_ancestor):
+            nonlocal ans
+
+            if not node:
+                return
+
+            ans = max(ans, max_ancestor - node.data)
+
+            max_ancestor = max(max_ancestor, node.data)
+
+            dfs(node.left, max_ancestor)
+            dfs(node.right, max_ancestor)
+
+        dfs(root.left, root.data)
+        dfs(root.right, root.data)
+
+        return ans
